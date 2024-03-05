c04fd78d3651513e719af942194a28dc8d4a21d8
Hiren <Hirenshah2550@gmail.com>
2024-03-05 22:50:05 +0530

    profile page complete

--- Property-Management/src/pages/Profile.jsx
+++ Property-Management/src/pages/Profile.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-md mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          src={currentUser.avatar}
+          alt="Profile"
+        />
+        <input
+          className=" border p-3 focus:outline-none rounded-lg "
+          type="text"
+          placeholder="username"
+          id="username"
+        />
+        <input
+          className=" border p-3 focus:outline-none rounded-lg "
+          type="email"
+          placeholder="email"
+          id="email"
+        />
+        <input
+          className=" border p-3 focus:outline-none rounded-lg "
+          type="text"
+          placeholder="password"
+          id="password"
+        />
+        <button className="bg-slate-700 uppercase text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 ">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
