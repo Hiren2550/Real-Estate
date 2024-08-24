60be2a4a05e1f888227475d3c3700c9d91dadc29
Hiren2550 <hirenshah2550@gmail.com>
2024-08-24 19:59:27 +0530

    bug fix

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,12 +1,12 @@
 import User from "../models/user.model.js";
-import bcyptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   // console.log(req.body);
   let { username, email, password } = req.body;
-  const hashedpassword = bcyptjs.hashSync(password, 10);
+  const hashedpassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedpassword });
   try {
     await newUser.save();
@@ -22,7 +22,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found !"));
-    const validPassword = bcyptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(404, "Wrong credentials !"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
@@ -47,7 +47,7 @@ export const google = async (req, res, next) => {
         .json(rest);
     } else {
       const generatedPassword = Math.random().toString(36).slice(-8);
-      const hashedpassword = bcyptjs.hashSync(generatedPassword, 10);
+      const hashedpassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
